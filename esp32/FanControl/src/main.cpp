854ff0f159e43f54378a7e9adc6f220312a6847a
Deivid Willy <deividwillydev@gmail.com>
2026-05-05 17:14:59 -0300

    Introduzinho FreeRTOS

--- esp32/FanControl/src/main.cpp
+++ esp32/FanControl/src/main.cpp
@@ -1,9 +1,5 @@
+#include "freertos/portmacro.h"
 #include <Arduino.h>
-
-uint8_t percentToPWM(uint8_t percent) {
-    if (percent > 100) percent = 100;
-    return (uint8_t)((percent / 100.0) * 255);
-}
 
 const uint8_t PWM_PIN = 19;
 const uint8_t PWM_CHANNEL = 0;
@@ -11,43 +7,62 @@ const uint8_t PWM_CHANNEL = 0;
 const uint16_t FREQUENCY = 25000;
 const uint8_t RESOLUTION = 8;
 
+uint8_t percentToPWM(uint8_t percent) {
+    if (percent > 100) percent = 100;
+    return (uint8_t)((percent / 100.0) * 255);
+}
+
 struct __attribute__((packed)) Packet {
     uint8_t temp;
     uint8_t load;
     uint8_t rpm;
 };
+volatile Packet packet;
 
-Packet packet;
+void taskSerial(void *pvParameters){
+    while (true){
+
+        if (Serial.available() >= sizeof(Packet)) {
+            Serial.println("Recebendo serial");
+
+            Serial.readBytes((uint8_t*)&packet, sizeof(Packet));
+
+            uint8_t pwm = percentToPWM(packet.rpm);
+
+            Serial.printf("Temp: %d | Load: %d | RPM: %d | DUTY: %d\n",
+                packet.temp,
+                packet.load,
+                packet.rpm,
+                pwm
+            );
+
+            Serial.println(uxTaskGetStackHighWaterMark(NULL));
+        }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
 void setup() {
     Serial.begin(115200);
+
     delay(1000);
 
     ledcSetup(PWM_CHANNEL, FREQUENCY, RESOLUTION);
     ledcAttachPin(PWM_PIN, PWM_CHANNEL);
 
+    xTaskCreatePinnedToCore(
+        taskSerial,
+        "RX Date",
+        2048,
+        NULL,
+        2,
+        NULL,
+        0
+    );
+
     Serial.println("ESP32 pronto!");
 }
 
 void loop() {
-
-    // 🔹 Espera receber os 3 bytes
-    if (Serial.available() >= sizeof(Packet)) {
-
-        Serial.readBytes((char*)&packet, sizeof(Packet));
-
-        uint8_t pwm = percentToPWM(packet.rpm);
-
-        ledcWrite(PWM_CHANNEL, pwm);
-
-        Serial.printf(
-            "Temp: %d | Load: %d | RPM: %d | PWM: %d\n",
-            packet.temp,
-            packet.load,
-            packet.rpm,
-            pwm
-        );
-      
-    }
-    delay(500);
 }
