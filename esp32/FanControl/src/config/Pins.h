3e9bff362665b10df191452e17b4d78c79cf9e7f
Deivid Willy <deividwillydev@gmail.com>
2026-06-29 13:59:42 -0300

    remap pins

--- esp32/FanControl/src/config/Pins.h
+++ esp32/FanControl/src/config/Pins.h
@@ -2,13 +2,13 @@
 
 namespace Pins {
     const uint8_t EPD_MOSI = 23;
-    const uint8_t EPD_SCK = 18;
+    const uint8_t EPD_SCK = 22;
     const uint8_t EPD_CS = 5;
     const uint8_t EPD_DC = 17;
     const uint8_t EPD_RES = 16;
-    const uint8_t EPD_BUSY = 25;
+    const uint8_t EPD_BUSY = 18;
 
-    const uint8_t PWM_PIN_0 = 19;
+    const uint8_t PWM_PIN_0 = 34;
     //const uint8_t PWM_PIN_1 = 0;
     //const uint8_t PWM_PIN_2 = 0;
 }
